fc5459ad295614d456ffcda5f0b23e2c6f1a6443
hsrivastava96 <harshit67@gmail.com>
2017-04-17 21:14:19 +0530

    BOTH DFS AND BFS RUNNING in one file

---
+++ DFSANDBFS/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=dfsandbfs
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/dfsandbfs
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=dfsandbfs.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/dfsandbfs.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=dfsandbfs
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/dfsandbfs
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=dfsandbfs.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/dfsandbfs.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
